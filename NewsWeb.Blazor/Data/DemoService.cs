dced349b85a11e03c32d43bcf9afba45cf0bdf27
TrungV <vindvinn@gmail.com>
2020-04-27 00:15:58 +0700

    Setup Demo

---
+++ NewsWeb.Blazor/Data/DemoService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsWeb.Blazor.Data
+{
+    public class DemoService
+    {
+        public Task<int> Ham()
+        {
+            return Task.FromResult(1);
+        }
+    }
+}
